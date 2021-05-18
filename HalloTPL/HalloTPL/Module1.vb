749706de3b37523ee618dc295a05d211586a2400
André <ruland@outlook.de>
2021-05-18 11:03:40 +0200

    TPL mit Continue

--- HalloTPL/HalloTPL/Module1.vb
+++ HalloTPL/HalloTPL/Module1.vb
@@ -14,8 +14,22 @@ Module Module1
         Dim t1 As New Task(Sub()
                                Console.WriteLine("T1 gestartet")
                                Thread.Sleep(1200)
+                               Throw New NotFiniteNumberException()
                                Console.WriteLine("T1 fertig")
                            End Sub)
+
+        t1.ContinueWith(Sub(t)
+                            Console.WriteLine("Continue T1 (immer)")
+                        End Sub)
+
+        t1.ContinueWith(Sub(t)
+                            Console.WriteLine("ERROR in T1  (nur bei Execption)")
+                            Console.WriteLine($"{vbTab}{t.Exception.InnerException.Message}")
+                        End Sub, TaskContinuationOptions.OnlyOnFaulted)
+
+        t1.ContinueWith(Sub(t)
+                            Console.WriteLine("OK in T1 (nur wenn OK)")
+                        End Sub, TaskContinuationOptions.OnlyOnRanToCompletion)
 
         Dim t2 As New Task(Of Long)(Function()
                                         Console.WriteLine("T2 gestartet")
