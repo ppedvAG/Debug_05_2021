28a31b7ecf9e0a5f3c3d6665f86afc9527e32217
André <ruland@outlook.de>
2021-05-18 10:08:21 +0200

    Paralell ist cool

---
+++ HalloTPL/HalloTPL/Module1.vb
@@ -0,0 +1,30 @@
+﻿Imports System.Threading
+
+Module Module1
+
+    Sub Main()
+        Console.WriteLine("Hallo TPL")
+
+        'Zähle()
+        'Zähle()
+        'Zähle()
+        'Console.WriteLine("------")
+        'Parallel.Invoke(AddressOf Zähle, AddressOf Zähle, AddressOf Zähle, AddressOf Zähle)
+        Parallel.For(0, 1000000, AddressOf ZeigeZahl)
+
+
+
+        Console.WriteLine("Ende")
+        Console.ReadLine()
+    End Sub
+
+    Sub Zähle()
+        For index = 1 To 10
+            ZeigeZahl(index)
+        Next
+    End Sub
+
+    Private Sub ZeigeZahl(index As Integer)
+        Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId}: {index}")
+    End Sub
+End Module
